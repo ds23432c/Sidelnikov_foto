fe4356ccff1d9210026b180585c807420e9872e2
Alex <shura.korostelev.93@mail.ru>
2026-05-05 22:29:30 +0300

    Serve static files through Django fallback

--- config/urls.py
+++ config/urls.py
@@ -14,5 +14,7 @@ urlpatterns = [
     path('admin-panel/', include('apps.admin_panel.urls')),
 ]
 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
